24e77c5bb4eceee805d72a3603635b354eb19fcc
imbdev <moose@inspiredmommyboss.com>
2017-05-10 17:35:10 +0800

    Update for 10 May 2017

--- template-homepage.php
+++ template-homepage.php
@@ -108,6 +108,136 @@ get_header(); ?>
 
 	</div>
 
-</section> <!-- End imb-home-top-roadmap -->
+</section> <!-- End imb-home-social-proof -->
+
+<section id="imb-home-hero-story" class="container-fluid">
+	
+	<div class="content-box container">
+
+		<h1 class="headline text-center">You have a Powerful Purpose! Let's start pursuing it!</h1>
+
+		
+			<article class="content">
+
+				<img src="/wp-content/uploads/2017/05/hero-right-img-400x610.jpg" alt="" class="pull-right">
+				<!-- <img src="http://lorempixel.com/400/600/abstract/1" alt="" class="pull-right"> -->
+				<!-- <img src="/wp-content/uploads/2017/05/hero-story-test-img-400x600.jpeg" alt="" class="pull-right"> -->
+
+
+				<h3><i>HELLO GORGEOUS!</i> I'M TIANA GUSTAFSON.</h3>
+				
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+
+				<p>
+					Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+
+				<h3>YOU FEEL LIKE YOU'RE PLAYING SOMEONE ELSE'S GAME</h3>
+
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+
+				<h3>YOU YEARN FOR SOMETHING MORE!</h3>
+
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. 
+				</p>
+
+
+			</article>
+
+
+		<!-- <div class="left-image-box col-sm-3 col-md-6 col-lg-6">
+			
+			<figure>
+				
+				
+			</figure>
+
+		</div> -->
+
+	</div>
+
+</section> <!-- End imb-home-hero-story -->
+
+<section id="imb-home-testimonial" class="container-fluid">
+	
+	<div class="content-box">
+		
+		<img class="bg-smudge-image" src="/wp-content/uploads/2017/05/the-moose-smudge-200KB-700x700.png" alt="">
+		
+		<div class="testimonial-block container">
+			
+			<div class="col-md-2 col-lg-2 col-sm-1 hidden-xs"></div>
+
+			<div class="col-md-8 col-lg-8 col-sm-9 col-xs-12">
+
+				<article class="testimonial container-fluid">
+					
+					<div class="image-box col-sm-3 col-md-3 col-lg-3">
+						<img class="author-image img-responsive" src="/wp-content/uploads/2016/01/Sharee-Moore-150x150.jpg" alt="">
+					</div>
+					<div class="text-box col-sm-9 col-md-9 col-lg-9">
+
+						<i class="fa fa-quote-left" aria-hidden="true"></i>
+
+						<h3>Exceeds expectations every time!</h3>
+
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+
+						<i class="fa fa-quote-right pull-right" aria-hidden="true"></i>
+
+						<div class="author-info">
+							<strong>Sharee Moore</strong> | Author & Speaker
+						</div>
+						
+					</div>
+
+
+				</article>
+				
+			</div>
+
+			<div class="col-md-2 col-lg-2 col-sm-1 hidden-xs"></div>
+
+
+		</div>
+
+	</div>
+
+</section> <!-- End imb-home-testimonial -->
+
+
+
+
+
+
+
+
+
+
 <?php
 get_footer();
